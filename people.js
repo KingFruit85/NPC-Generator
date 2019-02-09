2590d23f1f5a9e17ed4709e05b596108c45b3969
KingFruit85 <christopher.aaron.long@gmail.com>
2019-02-09 22:38:26 +0000

    refactored some functions, still improvements to be made.

--- people.js
+++ people.js
@@ -1,3 +1,5 @@
+//import files
+
 const bodyParts =require('./bodyParts.js');
 const clothes =require('./clothes.js');
 const descriptors =require('./descriptors.js');
@@ -5,70 +7,61 @@ const names =require('./names.js');
 const occupation =require('./occupations.js');
 const traits =require('./traits.js');
 const death =require('./death.js');
+const utils =require('./utils.js');
+
+//-----------------------
+
+//some generic properties
+
+const genders = ["male", "female"];
 
 
-var getRandomNumber = (min, max) => {return Math.floor(Math.random() * (max - min + 1)) + min;};
+//-----------------------
 
-
-var getRandomItem = (collection) => {index = getRandomNumber(0, collection.length -1);
-    return collection[index];
-};
 
 var getGender = (person, gender) => {
 
   if (gender === undefined){
 
-    x = getRandomNumber(1,2);
-    switch (true) {
-      case (x === 1):person.gender = "male";break;
-      case (x === 2):person.gender = "female";break;
-      default:person.gender = "fell though swith case"
+    person.gender = utils.getRandomItem(genders);
 
-  }
 }
-  else if (gender === "male"){
-      person.gender = "male";
-  }
-  else if (gender === "female"){
-    person.gender = "female";
-  }
-  else{
-    person.gender = "invalid gender arguement";
-  }
-    return person;
+    else if (gender === "male"){person.gender = "male";}
+    else if (gender === "female"){person.gender = "female";}
+    else{person.gender = "invalid gender arguement";}
+      return person;
   }
 
 
 var createName = (person) => {
   if (person.gender === "male"){
-    person.firstName = getRandomItem(names.maleNames);
-    person.lastName  = getRandomItem(names.lastNames);
-    return person;
+    person.firstName = utils.getRandomItem(names.maleNames);
+    person.lastName  = utils.getRandomItem(names.lastNames);
   }
-  else if (person.gender === "female"){
-    person.firstName = getRandomItem(names.femaleNames);
-    person.lastName  = getRandomItem(names.lastNames);
-    return person;
-  }
-  else return "Invalid Gender Supplied"
-}
+    else{
+      person.firstName = utils.getRandomItem(names.femaleNames);
+      person.lastName  = utils.getRandomItem(names.lastNames);
+      }
+      return person;
+    }
 
+//this function needs a rewrite, too complicated and doesnt catch errors
 var createAge = (person, category) => {
 
   if     (category === "infant"){person.age  = 1; person.ageCategory = "infant"}
-  else if(category === "child") {person.age  = getRandomNumber( 2 ,  15  ); person.ageCategory = "child";  return person;}
-  else if(category === "youth") {person.age  = getRandomNumber( 15 , 24  ); person.ageCategory = "youth";  return person;}
-  else if(category === "adult") {person.age  = getRandomNumber( 25 , 64  ); person.ageCategory = "adult";  return person;}
-  else if(category === "senior"){person.age  = getRandomNumber( 64 , 107 ); person.ageCategory = "senior"; return person;}
+  else if(category === "child") {person.age  = utils.getRandomNumber( 2 ,  15 ); person.ageCategory = "child";  return person;}
+  else if(category === "youth") {person.age  = utils.getRandomNumber( 15 , 24 ); person.ageCategory = "youth";  return person;}
+  else if(category === "adult") {person.age  = utils.getRandomNumber( 25 , 64 ); person.ageCategory = "adult";  return person;}
+  else if(category === "senior"){person.age  = utils.getRandomNumber( 64 , 99 ); person.ageCategory = "senior"; return person;}
   else if(category === undefined){
-    person.age = getRandomNumber( 1, 107 )
+    person.age = utils.getRandomNumber( 1, 99 );
     switch (true) {
       case ( person.age === 1 )                  :person.ageCategory = "infant";break;
       case ( person.age >1 && person.age <15 )   :person.ageCategory = "child";break;
       case ( person.age > 14 && person.age < 25 ):person.ageCategory = "youth";break;
       case ( person.age > 24 && person.age < 65 ):person.ageCategory = "adult";break;
       case ( person.age > 64 )                   :person.ageCategory = "senior";break;
-      default: age.category = "Fell though switch case statement"
+      default: age.category = "Fell though switch case statement";
       return person;
   }
   }
@@ -82,83 +75,90 @@ var createAge = (person, category) => {
 
 }
 
+
 var getMaritalStatus = (person) => {
 
-
   switch (true) {
-    case person.ageCategory === "infant"                              :person.maritalStatus = "single";break;
-    case person.ageCategory === "child"                               :person.maritalStatus = "single";break;
-    case person.ageCategory === "youth" && person.gender === "male"   :person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
-    case person.ageCategory === "youth" && person.gender === "female" :person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
-    case person.ageCategory === "adult" && person.gender === "male"   :person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
-    case person.ageCategory === "adult" && person.gender === "female" :person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
-    case person.ageCategory === "senior" && person.gender === "male"  :person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
-    case person.ageCategory === "senior" && person.gender === "female":person.maritalStatus = getRandomItem(["single","in a relationship","married","widower"]);break;
+    case person.ageCategory === "infant"                               :person.maritalStatus = "single";break;
+    case person.ageCategory === "child"                                :person.maritalStatus = "single";break;
+    case person.ageCategory === "youth"  && person.gender === "male"   :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widower"]);break;
+    case person.ageCategory === "youth"  && person.gender === "female" :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widow"]);break;
+    case person.ageCategory === "adult"  && person.gender === "male"   :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widower"]);break;
+    case person.ageCategory === "adult"  && person.gender === "female" :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widow"]);break;
+    case person.ageCategory === "senior" && person.gender === "male"   :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widower"]);break;
+    case person.ageCategory === "senior" && person.gender === "female" :person.maritalStatus = utils.getRandomItem(["single","in a relationship","married","widow"]);break;
     default:person.maritalStatus = "fell though switch statement"
 
   }
   return person;
 }
 
-var getTraits = (person) => { //need to create collection in person
 
-  person.positiveTrait = getRandomItem(traits.positiveTraits);
-  person.neutralTrait  = getRandomItem(traits.neutralTraits);
-  person.badTrait      = getRandomItem(traits.badTraits);
+var getTraits = (person) => {
+
+  person.positiveTrait = utils.getRandomItem(traits.positiveTraits);
+  person.neutralTrait  = utils.getRandomItem(traits.neutralTraits);
+  person.badTrait      = utils.getRandomItem(traits.badTraits);
 
   return person;
 }
 
+
 var getHeight = (person) => {
   person.height = {};
   switch (true) {
-    case ( person.age === 1 )                   : person.height.feet = 1; person.height.inches = getRandomNumber( 0 , 7 );break;
-    case ( person.age > 1  && person.age < 4 )  : person.height.feet = 2; person.height.inches = getRandomNumber( 6 , 11 );break;
-    case ( person.age > 3  && person.age < 9 )  : person.height.feet = 3; person.height.inches = getRandomNumber( 1 , 11 );break;
-    case ( person.age > 8  && person.age < 13)  : person.height.feet = 4; person.height.inches = getRandomNumber( 1 , 11 );break;
-    case ( person.age > 12 && person.age < 16 ) : person.height.feet = 5; person.height.inches = getRandomNumber( 1 , 8 );break;
-    case ( person.age > 16 )                    : person.height.feet = getRandomNumber( 5 , 6); person.height.inches = getRandomNumber( 1 , 11 );break;
+    case ( person.age === 1 )                   : person.height.feet = 1; person.height.inches = utils.getRandomNumber( 0 , 7  );break;
+    case ( person.age > 1  && person.age < 4 )  : person.height.feet = 2; person.height.inches = utils.getRandomNumber( 6 , 11 );break;
+    case ( person.age > 3  && person.age < 9 )  : person.height.feet = 3; person.height.inches = utils.getRandomNumber( 1 , 11 );break;
+    case ( person.age > 8  && person.age < 13)  : person.height.feet = 4; person.height.inches = utils.getRandomNumber( 1 , 11 );break;
+    case ( person.age > 12 && person.age < 16 ) : person.height.feet = 5; person.height.inches = utils.getRandomNumber( 1 , 8  );break;
+    case ( person.age > 16 )                    : person.height.feet = utils.getRandomNumber( 5 , 6); person.height.inches = utils.getRandomNumber( 1 , 11 );break;
     default:"Fell though switch case statement"
 
   }
   return person
 }
 
+
 var getOccupation = (person) => {
 
-  if (person.age < 15) {person.profession = "unemployed"}
-  else{person.profession = getRandomItem(occupation.occupation)}
+  if (person.age < 16) {person.profession = "unemployed"}
+  else if (person.age >= 65) {person.profession = "retired"}
+  else{
+    person.profession = utils.getRandomItem(occupation.occupation)
+  }
   return person;
 
 }
 
 var createFeatures = (person) =>{
 
-  person.eyeColour  = getRandomItem(["brown","hazel","blue", "green","silver","amber"]);
-  person.hairColour = getRandomItem(["brown","blond","black","auburn","red","gray","white"]);
-  person.hairLength = getRandomItem(["short","long","medium"]);
-  person.skinTone   = getRandomItem(["pale white", "fair", "light brown","olive","brown","dark brown","black"]);
+  person.eyeColour  = utils.getRandomItem(["brown","hazel","blue", "green","silver","amber"]);
+  person.hairColour = utils.getRandomItem(["brown","blond","black","auburn","red","gray","white"]);
+  person.hairLength = utils.getRandomItem(["short","long","medium"]);
+  person.skinTone   = utils.getRandomItem(["pale white", "fair", "light brown","olive","brown","dark brown","black"]);
 
   return person;
+
 }
+
 
 var createSpouse = (person) => {
 
-  if (person.ageCategory === "infant" || person.ageCategory === "child" || person.maritalStatus === "single"){
-    person.spouse = "no spouse";
+  if (person.ageCategory === "infant" || person.ageCategory === "child" || person.maritalStatus === "single")
+  {
 
-    return person;
+    person.spouse = "no spouse";
 
   }
   else if (person.gender === "male") {
 
     switch (true) {
+
       case person.ageCategory === "youth" : person.spouse = createSpousePerson(person, "female");break;
       case person.ageCategory === "adult" : person.spouse = createSpousePerson(person, "female");break;
       case person.ageCategory === "senior": person.spouse = createSpousePerson(person, "female");break;
       default:person.spouse = "fell though switch statement"
-
-      return person;
 
     }
 
@@ -171,29 +171,29 @@ var createSpouse = (person) => {
       case person.ageCategory === "senior": person.spouse = createSpousePerson(person, "male");break;
       default:person.spouse = "fell though switch statement"
 
-      return person;
-
     }
 
   }
 
+  return person;
 
 }
+
 
 var createSpousePerson = (person, gender) => {
 
   spouse = {}
 
-  if (person.maritalStatus === "widow" || person.maritalStatus === "widower"){
+  if (person.maritalStatus === "widow" || person.maritalStatus === "widower")
+  {
+
     spouse.status = "deceased"
     getGender(spouse, gender)
     createName(spouse);
     createAge(spouse, person.ageCategory);
-    relationshipLength = getRandomNumber(1 , (spouse.age - 16))
-    spouse.relationshipLength = relationshipLength - getRandomNumber(1 , relationshipLength)
-
-    spouse.causesOfDeath = getRandomItem(death.causesOfDeath);
-
+    relationshipLength = utils.getRandomNumber(1 , (spouse.age - 16))
+    spouse.relationshipLength = relationshipLength - utils.getRandomNumber(1 , relationshipLength)
+    spouse.causesOfDeath = utils.getRandomItem(death.causesOfDeath);
 
   }
   else{
@@ -208,11 +208,14 @@ var createSpousePerson = (person, gender) => {
     getOccupation(spouse);
     createFeatures(spouse);
     getTraits(spouse);
-    relationshipLength = getRandomNumber(1 , (spouse.age - 16))
-    spouse.relationshipLength = relationshipLength - getRandomNumber(1 , relationshipLength)
+    relationshipLength = utils.getRandomNumber(1 , (spouse.age - 16))
+    spouse.relationshipLength = relationshipLength - utils.getRandomNumber(1 , relationshipLength)
 
-    if (person.maritalStatus === "married" || person.maritalStatus === "widow" || person.maritalStatus == "widower"){
+    if (person.maritalStatus === "married" || person.maritalStatus === "widow" || person.maritalStatus == "widower")
+    {
+
         spouse.lastName = person.lastName;
+
     }
   }
 
@@ -220,7 +223,7 @@ var createSpousePerson = (person, gender) => {
 
 }
 
-//children cannot be older than the mother and the mother cannot have given birth
+//rewrite/refactor - too long, too confusing to read
 var createChild = (person) => {
 
   if (person.spouse != "no spouse" && person.spouse.gender != person.gender){
@@ -232,7 +235,7 @@ var createChild = (person) => {
       getGender(child);
       createName(child)
       child.lastName = getFamilySurname(person);
-      child.age = getRandomNumber(1 , (person.age - 16));
+      child.age = utils.getRandomNumber(1 , (person.age - 16));
       person.children.push(child)
     }
     return person;
@@ -247,7 +250,7 @@ var createChild = (person) => {
         getGender(child);
         createName(child)
         child.lastName = getFamilySurname(person);
-        child.age = getRandomNumber(1 , (person.age - 16));
+        child.age = utils.getRandomNumber(1 , (person.age - 16));
         person.adoptedChildren.push(child)
       }
       return person;
@@ -259,17 +262,25 @@ var createChild = (person) => {
 }
 
 var getFamilySurname = (person) => {
-  let familySurname = undefined
+
+  let familySurname = undefined;
+
   if (person.gender === "male"){
-    return person.lastName
+
+    return person.lastName;
+
   }
   else if (person.gender === "female"){
-    return person.spouse.lastName
+
+    return person.spouse.lastName;
+
   }
-  return familySurname
+
+  return familySurname;
 }
 
 
+//this probably needs a refactor, must be a cleaner way to handle the age category arguements
 var createPerson = (category) => {
 
   person = {};
@@ -378,5 +389,6 @@ var createMultiplePeople = (number) =>{
 // console.log(xy)
 
 // console.log(JSON.stringify(createMultiplePeople(100), undefined, 2))
+console.log(createPerson(""))
 
 exports.createPerson = createPerson;
