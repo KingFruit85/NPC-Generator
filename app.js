b7e3d2442329780ead5f7c3e42f667f5a1de7e80
KingFruit85 <christopher.aaron.long@gmail.com>
2019-02-10 21:03:34 +0000

    refactored the write functions, added a read function

--- app.js
+++ app.js
@@ -2,44 +2,65 @@
 
 const people = require('./people.js');
 const places = require('./places.js');
-
-
 const mongo =require('mongodb');
+const url = "mongodb://localhost:27017/npc_generator";
+const MongoClient = require('mongodb').MongoClient;
+
+
+
+
+var dbInsert = (table, request) => {
+
+  MongoClient.connect(url, function(err, db) {
+
+    if (err) throw err;
+    var dbo = db.db("npc_generator");
+    dbo.collection(table).insertOne(request, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+
+}
+
 
 
 var insertPersonToDB = (args) => {
 
+  dbInsert("people", people.createPerson());
 
-var url = "mongodb://localhost:27017/npc_generator"
-var MongoClient = require('mongodb').MongoClient;
+}
 
-MongoClient.connect(url, function(err, db) {
+var insertBuildingToDB = (args) => {
+
+  dbInsert("homes", places.populateHouse());
+
+}
+
+var insertResPlaceInDB = (args) => {
+
+  dbInsert("places", places.populateResidentialPlace());
+
+}
+
+var dbFind = () => {
+
+  MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("npc_generator");
-  dbo.collection("people").insertOne(people.createPerson(args), function(err, res) {
+  dbo.collection("people").find({}, { projection: { _id: 0, firstName: 1, lastName: 1 }} ).toArray(function(err, result) {
     if (err) throw err;
-    console.log("1 document inserted");
+    console.log(result);
     db.close();
   });
 });
 
 }
 
-var insertBuildingToDB = (args) => {
 
-  var url = "mongodb://localhost:27017/npc_generator"
-  var MongoClient = require('mongodb').MongoClient;
+// insertPersonToDB();
+// insertBuildingToDB()
+// insertResPlaceInDB();
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("npc_generator");
-    dbo.collection("places").insertOne(places.populateHouse(args), function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  });
-}
-
-insertPersonToDB();
-insertBuildingToDB()
+dbFind();
